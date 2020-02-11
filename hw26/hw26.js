699a1ae87cbc5264a18ef835683eb5cb3d2b255a
webbadforya <didjoni1@gmail.com>
2020-02-11 18:01:02 +0200

    first commit

---
+++ hw26/hw26.js
@@ -0,0 +1,7 @@
+/^[a-z]{5,20}@[a-z]{1,20}.[a-z]{1,10}$/.test("boriapetrov@mail.ru");
+
+"/product/20".replace(/[a-z/]/g, "");
+
+"/person/10/device/90".match(/[\d]{1,100}/g);
+
+/^[\d]{1,1000}[ghtx].$/g.test("1g.");
